e2d125432c508781a1a60915af9d8f0d46eaff55
DimaLom <lom797@gmail.com>
2020-07-04 14:33:05 +0700

    restyling project, add auth

--- Students/Dmitriy_Lomov/project/server/Utils/dbfiller/index.js
+++ Students/Dmitriy_Lomov/project/server/Utils/dbfiller/index.js
@@ -1,49 +1,51 @@
-const db = require("mongoose");
-const fs = require("fs");
+let db = require('mongoose');
+let fs = require('fs');
 
-const catalog = "./server/Utils/dbfiller/catalog.json";
-const basket = "./server/Utils/dbfiller/basket.json";
+let catalog = './server/Utils/dbfiller/catalog.json';
+let basket = './server/Utils/dbfiller/basket.json';
 
-const Products = require("../../db/models/products.js");
+let Products = require('../../db/models/products.js');
 
-db.connect("mongodb://localhost/geekshop", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+db.connect('mongodb://localhost/geekshop', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
+//1 - read JSON
+//2 - create Model
+//3 - write to DB
+
 function readJSON(urlFile) {
-  return new Promise((res, rej) => {
-    fs.readFile(urlFile, "utf-8", (err, data) => {
-      if (!err) {
-        res(JSON.parse(data));
-      } else {
-        rej("Error reading file: " + urlFile);
-      }
+    return new Promise((res, rej) => {
+        fs.readFile(urlFile, 'utf-8', (err, data) => {
+            if (!err) {
+                res(JSON.parse(data));
+            } else {
+                rej('Error reading file: ' + urlFile);
+            }
+        })
     });
-  });
-}
+};
 
 async function writeCollection(array) {
-  await array.forEach(async (item) => {
-    try {
-      const newProduct = await Products.create({
-        name: item.product_name,
-        price: item.price,
-        // img: item.img ? item.img : 'default',
-        img: item.img || "default",
-        type: item.type,
-      });
-      console.log(newProduct.name + " added");
-    } catch (err) {
-      console.log("Err filling in writing Model: " + err);
-    }
-  });
+    await array.forEach(async item => {
+        try {
+            let newProduct = await Products.create({
+                name: item.product_name,
+                price: item.price,
+                // img: item.img ? item.img : 'default',
+                img: item.img || 'default'
+            });
+            console.log(newProduct.name + ' added');
+        }
+        catch(err) {
+            console.log('Err filling in writing Model: ' + err);
+        }
+    })
 }
 
 readJSON(catalog)
-  .then((d) => {
-    writeCollection(d);
-  })
-  .catch((d) => {
-    console.log(d);
-  });
+    .then(d => {
+        writeCollection(d)
+    })
+    .catch(d => {console.log(d)})
--- Students/Dmitriy_Lomov/project/server/routers/basket-router.js
+++ Students/Dmitriy_Lomov/project/server/routers/basket-router.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const fs = require('fs');
-const logger = require('../Utils/logger');
-const writer = require('../Utils/writer');
-const basketServices = require('../Services/basket');
+let express = require('express');
+let fs = require('fs');
+let logger = require('../Utils/logger');
+let writer = require('../Utils/writer');
+let basketServices = require('../Services/basket');
 
 
-const router = express.Router();
+let router = express.Router();
 
 router.get('/', (req, res) => {
     fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
         if (!err) {
-            const { newBasket } = basketServices.add(JSON.parse(data), req.body);
+            let { newBasket } = basketServices.add(JSON.parse(data), req.body);
             writer('./server/db/basket.json', newBasket)
                 .then(status => {
                     if (status) {
@@ -36,7 +36,7 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
     fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
         if (!err) {
-            const { newBasket, name } = basketServices.change(JSON.parse(data), req.params.id, req.body.amount);
+            let { newBasket, name } = basketServices.change(JSON.parse(data), req.params.id, req.body.amount);
             writer('./server/db/basket.json', newBasket)
                 .then(status => {
                     if (status) {
@@ -53,7 +53,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
     fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
         if (!err) {
-            const { newBasket, name } = basketServices.delete(JSON.parse(data), req.params.id);
+            let { newBasket, name } = basketServices.delete(JSON.parse(data), req.params.id);
             writer('./server/db/basket.json', newBasket)
                 .then(status => {
                     if (status) {
