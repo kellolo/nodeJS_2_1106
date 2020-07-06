acf25f9f604aedf0f87847fbf5f3ffeb691c8940
DimaLom <lom797@gmail.com>
2020-07-06 18:01:57 +0700

    add remove functionality and some fixes

--- Students/Dmitriy_Lomov/project/src/App.vue
+++ Students/Dmitriy_Lomov/project/src/App.vue
@@ -20,19 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #dfe7ec;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- Students/Dmitriy_Lomov/project/src/containers/Basket.vue
+++ Students/Dmitriy_Lomov/project/src/containers/Basket.vue
@@ -1,81 +1,105 @@
 <template>
-    <div class="cart-block ">
-        <div class="d-flex">
-            <strong class="d-block">Всего товаров</strong>
-            <div id="quantity"></div>
-        </div>
-        <hr>
-        <div class="cart-items">
-            <item v-for="item of items" :key="item.id_product" :type="'basket'" :item="item"/>
-        </div>
-        <hr>
-        <div class="d-flex">
-            <strong class="d-block">Общая ст-ть:</strong>
-            <div id="price"></div>
-        </div>
+  <div class="cart-block p-3">
+    <div class="d-flex">
+      <strong class="d-block">Всего товаров: {{ this.totalAmount }}</strong>
     </div>
+    <hr />
+    <div class="cart-items d-flex flex-wrap">
+      <item
+        v-for="item of items"
+        :key="item.id_product"
+        :type="'basket'"
+        :item="item"
+      />
+    </div>
+    <hr />
+    <div class="d-flex">
+      <strong class="d-block">Общая ст-ть: {{ this.totalPrice }}</strong>
+    </div>
+  </div>
 </template>
 
 <script>
-import item from '../components/Item.vue'
+import item from "../components/Item.vue";
 
 export default {
-    components: { item },
-    data() {
-        return {
-            items: [],
-            url: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json',
-        }
-    },
-    mounted() {
-        this.$parent.get('/api/basket/').then(d => {
-            this.items = d.contents;
-        })
-    },
-    methods: {
-        add(item) {
-            let find = this.items.find(el => el.id_product == item.id_product);
+  components: { item },
+  data() {
+    return {
+      items: [],
+      totalPrice: 0,
+      totalAmount: 0,
+    };
+  },
+  mounted() {
+    this.$parent.get("/api/basket" + this.$route.path).then((d) => {
+      this.items = d.items;
+      this.items.forEach((el) => {
+        this.totalPrice += el.price * el.amount;
+        this.totalAmount += el.amount;
+      });
+    });
+  },
+  methods: {
+    add(item) {
+      let find = this.items.find((el) => el._id == item._id);
 
-            if (!find) {
-                let newItem = Object.assign({}, item, { quantity: 1 });
-                this.$parent.post('/api/basket/', newItem)
-                    .then(res => {
-                        if (res.status) {
-                            this.items.push(newItem);
-                        }
-                    });
-            } else {
-                this.$parent.put(`/api/basket/${item.id_product}`, { amount: 1 })
-                    .then(res => {
-                        if (res.status) {
-                            find.quantity++
-                        }
-                    });
+      if (!find) {
+        let newItem = Object.assign({}, item, { amount: 1 });
+        this.$parent
+          .post("/api/basket" + this.$route.path, newItem)
+          .then((res) => {
+            if (res.status) {
+              this.items.push(newItem);
+              this.totalPrice += newItem.price;
+              this.totalAmount++;
             }
-        },
-        remove(item) {
-            let find = this.items.find(el => el.id_product == item.id_product);
+          });
+      } else {
+        this.$parent
+          .put("/api/basket" + this.$route.path, { amount: 1, _id: find._id })
+          .then((res) => {
+            if (res.status) {
+              find.amount++;
+              this.totalPrice += find.price;
+              this.totalAmount++;
+            }
+          });
+      }
+    },
+    remove(item) {
+      let find = this.items.find((el) => el._id == item._id);
+      this.totalPrice -= find.price;
+      this.totalAmount--;
 
-            if (find.quantity == 1) {
-                this.$parent.delete(`/api/basket/${item.id_product}`)
-                    .then(res => {
-                        if (res.status) {
-                            this.items.splice(this.items.indexOf(find), 1);
-                        }
-                    });
-            } else {
-                this.$parent.put(`/api/basket/${item.id_product}`, { amount: -1 })
-                    .then(res => {
-                        if (res.status) {
-                            find.quantity--
-                        }
-                    });
+      if (find.amount == 1) {
+        this.$parent.delete("/api/basket" + this.$route.path).then((res) => {
+          if (res.status) {
+            this.items.splice(this.items.indexOf(find), 1);
+          }
+        });
+      } else {
+        this.$parent
+          .put("/api/basket" + this.$route.path, { amount: -1, _id: find._id })
+          .then((res) => {
+            if (res.status) {
+              find.amount--;
             }
-        }
-    }
-}
+          });
+      }
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.cart-block {
+  position: absolute;
+  background: rgba(0, 0, 0, 0.8);
+  top: 120px;
+  right: 1px;
+  border: 1px solid #ea1772;
+  border-radius: 5px;
+  transition: all ease 0.3s;
+}
 </style>
